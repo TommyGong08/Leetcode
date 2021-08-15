7ceb67fdc0453cc2dc70991428326ddd69fadd22
TommgGong08 <1034231958@qq.com>
2021-08-15 22:03:45 +0800

    add Hard Algorithm

---
+++ Algorithm/Hard/124.二叉树中的最大路径和.cpp
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=124 lang=cpp
+ *
+ * [124] 二叉树中的最大路径和
+ *
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ *
+ * algorithms
+ * Hard (44.17%)
+ * Likes:    1162
+ * Dislikes: 0
+ * Total Accepted:    145.1K
+ * Total Submissions: 328.2K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ * 
+ * 路径和 是路径中各节点值的总和。
+ * 
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目范围是 [1, 3 * 10^4]
+ * -1000 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {  
+private:
+    int res = INT_MIN;
+public:
+    int dfs(TreeNode* root){
+        //
+        if(root == nullptr) return 0;
+        int left_sum = max(0,dfs(root->left));
+        int right_sum = max(0,dfs(root->right));
+
+        //该节点的最大值
+        int node_sum = root->val + left_sum + right_sum; 
+        res = max(res, node_sum);  //更新答案
+
+        return (root->val + max(left_sum, right_sum));
+
+    }
+    int maxPathSum(TreeNode* root) {
+        int ans = dfs(root);
+        return max(res,ans);
+    }
+};
+// @lc code=end
+
