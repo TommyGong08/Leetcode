48498d1395eeb9ecbf4f937e798f1eed90fbaedb
TommgGong08 <1034231958@qq.com>
2021-08-15 22:21:26 +0800

    add Leetcode 128

---
+++ Algorithm/Medium/128.最长连续序列.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=cpp
+ *
+ * [128] 最长连续序列
+ *
+ * https://leetcode-cn.com/problems/longest-consecutive-sequence/description/
+ *
+ * algorithms
+ * Medium (54.20%)
+ * Likes:    875
+ * Dislikes: 0
+ * Total Accepted:    153.5K
+ * Total Submissions: 283.3K
+ * Testcase Example:  '[100,4,200,1,3,2]'
+ *
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * 
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^9 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        int ans = 1;
+        //排序
+        sort(nums.begin(), nums.end());
+        int curr_len = 1;
+        for(int i = 1; i < nums.size(); i++){
+            //如果等于钱一个属
+            if(nums[i] == nums[i-1]) continue;
+            else if((nums[i] - nums[i-1]) == 1)
+            {
+                curr_len +=1;
+                ans = max(ans, curr_len);
+            }
+            else{
+                //不是相邻的,临时长度从头再来
+                curr_len = 1;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
